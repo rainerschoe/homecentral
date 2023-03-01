2c82c89e2a6bb94b95dabef5b238210a4f4dba01
Rainer <rainer@wwad.de>
2023-03-01 20:32:27 +0100

    failed grpc attempt

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,44 @@
 pub mod data_lake;
 use data_lake::*;
+//use tokio_stream::stream_ext::StreamExt;
+//use bus::MessagePackBusAccess;
+//use bus::ConnectionSetup;
 
 
+pub mod bus {
+    tonic::include_proto!("bus");
+}
+//use futures::stream::Stream;
+use tokio_stream::StreamExt;
+
+async fn receive_from_bus_and_publish(datalake: TDataLake, server_url: String, publish_base_path: String)
+{
+    let mut client = bus::message_pack_bus_access_client::MessagePackBusAccessClient::connect(server_url).await.unwrap();
+
+    //let request = tonic::Request::new(bus::ConnectionSetup {
+    //    rx_mask: "255:255".into(),
+    //    remote_address: "255:255".into(),
+    //});
+    let req = bus::ConnectionSetup {
+        rx_mask: "255:255".into(),
+        remote_address: "200:1".into(),
+    };
+    let response = client.receive(req).await.unwrap();
+
+    let mut resp_stream = response.into_inner();
+    while let Some(received) = resp_stream.next().await {
+        let received = received.unwrap();
+        println!("\treceived message: `{:?}`", received);
+    }
+
+
+}
 
 #[tokio::main]
 async fn main() -> ()
 {
+    let mut datalake = TDataLake::new();
+    receive_from_bus_and_publish(datalake, "ipv4://192.168.0.200:50051".into(), "bus/receive/ug".into()).await;
 }
 
 #[tokio::test]
